b66db7cbc68c8507c0aae0c8d512b1e813f343d5
Anurag <anuraag.cas@gmail.com>
2021-07-30 22:11:08 +0530

    Disabled next button on last question

--- Script/Equiz.js
+++ Script/Equiz.js
@@ -1,128 +1,145 @@
-window.onload = function(){
+window.onload = function () {
     show(0);
 }
 let questions = [
     {
-    id: 1,
-    question: "What is the capital of India",
-    answer: "New Delhi",
-    option: [
-        "Uttar Pradesh",
-        "New Delhi",
-        "Rajasthan",
-        "Madhya Pradesh",
+        id: 1,
+        question: "Which type of JavaScript language is ?",
+        answer: "Object-Based",
+        option: [
+            "Object-Oriented",
+            "Object-Based",
+            "Assembly-language",
+            "High-Level",
 
-    ]
-},
-{
-    id: 2,
-    question: "What is the capital of Uttar Pradesh",
-    answer: "Lucknow",
-    option: [
-        "Uttar Pradesh",
-        "New Delhi",
-        "Lucknow",
-        "Madhya Pradesh",
+        ]
+    },
+    {
+        id: 2,
+        question: " Which tag is used to write the javascript code",
+        answer: "script",
+        option: [
+            "script",
+            "sp",
+            "javascript",
+            "java",
 
-    ]
-},
-{
-    id: 3,
-    question: "What is the name of Prime Minister of India",
-    answer: "Narendra Modi",
-    option: [
-        "Narendra Modi",
-        "Amit Shah",
-        "Rajnath Singh",
-        "Kejrival",
+        ]
+    },
+    {
+        id: 3,
+        question: "Original Name of Javascript is",
+        answer: "LiveScript",
+        option: [
+            "Mocha",
+            "Escript",
+            "LiveScript",
+            "Javascript",
 
-    ]
-}
+        ]
+    },
+    {
+        id: 4,
+        question: "Javascript is which type of language",
+        answer: "Interpreted",
+        option: [
+            "Interpreted",
+            "Compiled",
+            "Both",
+            "None of the above",
+
+        ]
+    }
 
 ];
 var question_count = 0;
-let optionSelected = [0, 0, 0];
+let optionSelected = [0, 0, 0, 0];
 
 
-function marks_count(){
-        let selectedOption = document.querySelector("li.option.active");
-        if(selectedOption === null) {
-            optionSelected.splice(question_count, 1, 0);
-        }
-        //let marks_count = document.querySelector("li.option.active").innerHTML;
-        else if(selectedOption.innerHTML == questions[question_count].answer ){
+function marks_count() {
+    let selectedOption = document.querySelector("li.option.active");
+    if (selectedOption === null) {
+        optionSelected.splice(question_count, 1, 0);
+    }
+    //let marks_count = document.querySelector("li.option.active").innerHTML;
+    else if (selectedOption.innerHTML == questions[question_count].answer) {
         optionSelected.splice(question_count, 1, 1);
-        }else{
+    } else {
         optionSelected.splice(question_count, 1, -1);
-        }
-        let marks = 0;
-        for(let i =0; i<optionSelected.length; i++){
-        if(optionSelected[i] == 1){
+    }
+    let marks = 0;
+    for (let i = 0; i < optionSelected.length; i++) {
+        if (optionSelected[i] == 1) {
             marks += 2;
-        }else if(optionSelected[i] == -1){
+        } else if (optionSelected[i] == -1) {
             marks -= 1;
         }
     }
-    sessionStorage.setItem("marks", marks);    
-} 
+    sessionStorage.setItem("marks", marks);
+}
 
 
-function final_page(){
+function final_page() {
     //if(question_count == questions.length - 1){
-        location.href = "EfinalPage.html";
-        
+    location.href = "EfinalPage.html";
+
     //}
 }
 
-function store(){
+function store() {
     let optionSelected = document.querySelector("li.option.active");
     const new_data = optionSelected == null ? null : optionSelected.innerHTML;
     //var new_data = document.querySelector("li.option.active").innerHTML;
-    if(sessionStorage.getItem('data')==null){
+    if (sessionStorage.getItem('data') == null) {
         sessionStorage.setItem('data', '[]');
-    }   
-    var old_data = JSON.parse(sessionStorage.getItem('data'));   
+    }
+    var old_data = JSON.parse(sessionStorage.getItem('data'));
     //old_data.push(new_data);
     old_data.splice(question_count, 1, new_data);
     sessionStorage.setItem('data', JSON.stringify(old_data));
 }
 
-function nextQue(){
 
 
-        marks_count();
-        store();
-        if(question_count<questions.length){
+function nextQue() {
+
+    //marks_count();
+    //store();
         question_count++;
         show(question_count);
-        }
+        console.log(question_count);
+    
     //marks_count();
     //show(question_count);
 }
 
-function prev(){
+function prev() {
 
-    if(question_count>0){
-        question_count--;        
+    if (question_count > 0) {
+        question_count--;
     }
     show(question_count);
-} 
+    
+        document.getElementById('nextButton').disabled = false;
+    
+}
 
-    
-function submit(){        
-    
+
+function submit() {
+
     final_page();
-    
+
 
 }
-function show(count){
-    if(count >= questions.length){
-        question_count = questions.length-1;
-     return;   
+function show(count) {
+    if (count >= questions.length) {
+        question_count = questions.length - 1;
+        document.getElementById('nextButton').disabled = true;
+        return;
     }
     let question = document.getElementById('questions');
-    question.innerHTML = 
-    `<h2>Que${question_count + 1}.  ${questions[count].question}</h2>
+    question.innerHTML =
+        `<h2>Que${question_count + 1}.  ${questions[count].question}</h2>
     <ul class="option_list">
         <li class="option">${questions[count].option[0]}</li>
         <li class="option">${questions[count].option[1]}</li>
@@ -133,27 +150,26 @@ function show(count){
     toggelVisibility();
 }
 
-function toggelVisibility(){
+function toggelVisibility() {
     let option = document.querySelectorAll("li.option");
-    for(let i = 0; i<option.length; i++){
-        option[i].onclick = function add_active(){
-            for(j=0; j<option.length; j++){
-                if(option[j].classList.contains("active")){
+    for (let i = 0; i < option.length; i++) {
+        option[i].onclick = function updateSelectedOption() {
+            for (j = 0; j < option.length; j++) {
+                if (option[j].classList.contains("active")) {
                     option[j].classList.remove("active");
                 }
             }
             option[i].classList.add("active");
+            marks_count();
+            store();
         }
     }
 }
-
-
-function submitform(e){
+function submitform(e) {
     e.preventDefault();
     let name = document.forms["front_name"]["enter_name"].value;
     //let name = document.getElementById('Enter_Name').value;
 
     sessionStorage.setItem("Name", name)
-    location.href ="Equiz.html";
-    
+    location.href = "Equiz.html";
 }
--- Script/Etimer.js
+++ Script/Etimer.js
@@ -1,8 +1,7 @@
-const time_limit = 1;
+const time_limit = 2;
 let time = time_limit * 60;
 
 let countdown = document.getElementById('timer');
-
 let inter = setInterval(updateCountdown, 1000)
 
 function updateCountdown(){
@@ -13,11 +12,11 @@ function updateCountdown(){
     minutes = minutes<10 ? '0'+ minutes : minutes;
     seconds = seconds<10 ? '0'+ seconds : seconds;
 
-    countdown.innerHTML = `${minutes}: ${seconds}`
+    countdown.innerHTML = `${minutes} : ${seconds}`
     time--;
     }
     else{
         clearInterval(inter);
-        //location.href="EfinalPage.html"
+        location.href="EfinalPage.html"
     }
 }
